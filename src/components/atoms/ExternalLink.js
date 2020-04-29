2c0d0259ed2b2a076d1c8d117bed55f7fcfc4d45
crhallen <7114281+crhallen@users.noreply.github.com>
2020-04-29 20:17:39 +0100

    fix external link

--- src/components/atoms/ExternalLink.js
+++ src/components/atoms/ExternalLink.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import styled from 'styled-components'
 import {OutboundLink} from 'gatsby-plugin-google-analytics'
 
-const ExternalLink = styled(OutboundLink).attrs({
+const ExternalLink = styled(({href}) => <OutboundLink href={href} />).attrs({
   target: '_blank',
   rel: 'noopener noreferrer',
 })``
